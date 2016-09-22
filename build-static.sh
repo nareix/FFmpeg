3b7698a0d108883135d216bde42fdf3eb93e3dab
nareix <hi.nareix@gmail.com>
2016-09-22 16:07:32 +0800

    static build

---
+++ build-static.sh
@@ -0,0 +1,4 @@
+
+./configure --disable-indevs --disable-outdevs --extra-version=static --extra-cflags=--static --enable-static --disable-shared --disable-ffplay --disable-ffserver \
+	--extra-ldexeflags="-static"
+
