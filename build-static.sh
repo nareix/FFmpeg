418a66d7ed90d2e085aab1e591ff68b0a709581a
nareix <hi.nareix@gmail.com>
2016-10-01 20:01:26 +0800

    change static

--- build-static.sh
+++ build-static.sh
@@ -1,4 +1,7 @@
 
-./configure --disable-indevs --disable-outdevs --extra-version=static --extra-cflags=--static --enable-static --disable-shared --disable-ffplay --disable-ffserver \
-	--extra-ldexeflags="-static"
+./configure --enable-librtmp --disable-indevs --disable-outdevs --extra-version=static \
+	--extra-cflags=--static \
+	--enable-static \
+	--disable-shared --disable-ffplay --disable-ffserver \
+	--extra-ldexeflags="-static -ldl"
 
